c02aa2f0f082747d460d9bddff40a37a600de8e1
MetalSnow <yasuotrend@gmail.com>
2024-03-13 17:27:05 +0800

    Add function to generate homepage content

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,25 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "index.js",
+  entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devtool: "inline-source-map",
+  mode: "development",
   plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
